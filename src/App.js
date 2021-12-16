1cca9ccce20cc42fbc8c53b6bc6a09f1eb186ab3
kuttalam <kuttalambc@gmail.com>
2021-12-16 16:39:22 +0530

    victory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Pricing from './Pricing';
+import './Pricing.css';
+
+function App () {
+return(
+  <div> hello</div>
+)
+  
+  
+}
+
+export default App;
